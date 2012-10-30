ddc23c1acd08e0e954055e109660736639437ab1
owenwj <owenwjowenwj@gmail.com>
2012-10-30 14:09:21 +0800

    the same as last change

--- Model/UTMember.h
+++ Model/UTMember.h
@@ -12,7 +12,7 @@
 {
     NSString            *memberID; // 会员号
     NSString            *memberName; // 会员名
-    // 会员头像
+    UIImage             *memberImage;// 会员头像
     NSString            *memberEmail; // 会员邮箱
     NSString            *memberEncryptedPassword; // 会员密码
     NSMutableDictionary *memberExtensions; // 其他信息
